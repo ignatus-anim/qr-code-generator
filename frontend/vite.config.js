90a3feebcdfc8089df39085dba18e17e68e1e995
godcandidate <godcandidate101@gmail.com>
2025-03-12 11:44:18 +0000

    feat: added a express server
    - This makes our app dynamic and suitable for Elastic beanstalk

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../frontend/server/dist', // Output the build files to the server/build directory
+    emptyOutDir: true,         // Clear the output directory before building
+  },
 })
