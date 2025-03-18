240ae95045a7d3238a6faa3ad7ada011d8bb04c4
godcandidate <godcandidate101@gmail.com>
2025-03-18 16:45:22 +0000

    chore: dockerize application
    - Added docker file and ignore file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../frontend/server/dist', // Output the build files to the server/build directory
-    emptyOutDir: true,         // Clear the output directory before building
+    emptyOutDir: true, // Clear the output directory before building
   },
-})
+});
