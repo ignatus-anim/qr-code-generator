68f2cf4691b3777b97fb3d3f2695708afd7f0aab
godcandidate <godcandidate101@gmail.com>
2025-03-18 16:26:44 +0000

    chore: remove lint on build files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,12 +4,28 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: [
+      'dist/**',
+      'server/dist/**',
+      'node_modules/**',
+      'coverage/**',
+      '__mocks__/**',
+      '*.config.js',
+      '*.config.cjs',
+      '*.setup.js',
+    ],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -24,10 +40,11 @@ export default [
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-undef': 'error',
+      'no-prototype-builtins': 'off',
+      'no-empty': 'warn',
+      'no-useless-escape': 'warn',
     },
   },
 ]
